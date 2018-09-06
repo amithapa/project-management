a35cb4da75eab5a1fa30f7ca595c923dbb7d810b
Dev <amithapa1994@gmail.com>
2018-09-07 00:50:44 +0530

    Base Directory

--- src/ProjectManagementBase.py
+++ src/ProjectManagementBase.py
@@ -2,12 +2,12 @@ from googleapiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
 import gspread
-from ProjectManagementConstants import CREDENTIALS_URL, READ_WRITE_SCOPES
+from constants import CREDENTIALS_URL, READ_WRITE_SCOPES
 
 class ProjectManagementBase:
     def __init__(self, scope=READ_WRITE_SCOPES):
         self.__creds = None
-        self.__scope = READ_WRITE_SCOPES
+        self.__scope = scope
         self.__setup()
 
     @property
@@ -19,5 +19,5 @@ class ProjectManagementBase:
         store = file.Storage(f'{CREDENTIALS_URL}/token.json')
         self.__creds = store.get()
         if not self.__creds or self.__creds.invalid:
-            flow = client.flow_from_clientsecrets(f'{CREDENTIALS_URL}/credentials.json', READ_WRITE_SCOPES)
+            flow = client.flow_from_clientsecrets(f'{CREDENTIALS_URL}/credentials.json', self.__scope)
             self.__creds = tools.run_flow(flow, store)
