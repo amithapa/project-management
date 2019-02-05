ed0d7cca46e0ac7a631f18cc43d4478acd84cfa1
Amit Thapa <amithapa1994@gmail.com>
2019-02-05 22:55:11 +0530

    Applying custom formating to the Sheet Data

--- src/spreadsheet_service_base.py
+++ src/spreadsheet_service_base.py
@@ -22,7 +22,6 @@ class SpreadSheetServiceBase(CredentialsBase):
     def selected_worksheet(self):
         return self.__selected_worksheet
 
-
     def open_spreadsheet_by_key(self, spreadsheet_key: str) -> None:
         try:
             self.__spreadsheet = self.__gs_authorize.open_by_key(spreadsheet_key)
@@ -58,3 +57,57 @@ class SpreadSheetServiceBase(CredentialsBase):
 
     def get_all_values(self) -> list:
         return self.__selected_worksheet.get_all_values()
+
+    def create_spread_sheet(self, sheet_title: str, rows: str="100", cols: str="20"):
+        self.__selected_worksheet = self.__spreadsheet.add_worksheet(title=sheet_title, rows=rows, cols=cols)
+
+    def get_cell_by_label(self, label: str) -> gspread.models.Cell:
+        return self.__selected_worksheet.acell(label)
+
+    def get_cell_by_index(self, row: int, col: int) -> gspread.models.Cell:
+        return self.__selected_worksheet.cell(row, col)
+
+    def update_cell_by_label(self, label: str, value: str) -> None:
+        self.__selected_worksheet.update_acell(label, value)
+        return None
+
+    def update_cell_by_index(self, row: int, col: int, value: str) -> None:
+        self.__selected_worksheet.update_cell(row, col, value)
+        return None
+
+    def __get_range(self, startRow: int=0, endRow: int=0, startCol: int=0, endCol: int=0):
+        return {
+            "sheetId": self.__selected_worksheet.id,
+            'startRowIndex': startRow,
+            'endRowIndex': endRow,
+            'startColumnIndex': startCol,
+            "endColumnIndex": endCol
+        }
+
+    def apply_formatting_to_cell(self, cell_range: dict, cell: dict, fields: dict):
+        """
+        Refer: https://developers.google.com/sheets/api/samples/formatting
+        :param cell_range:
+        :param cell:
+        :param fields:
+        :return:
+        """
+        payload_data = {
+            'requests': [
+                {
+                    'repeatCell': {
+                        'range': cell_range,
+                        'cell': cell,
+                        'fields': fields
+                    }
+                }
+            ]
+        }
+        self.__selected_worksheet.spreadsheet.batch_update(payload_data)
+
+    def bold_range(self, startRow, endRow: int, startCol: int, endCol: int):
+        cell_range = self.__get_range(startRow, endRow, startCol, endCol)
+        cell = {'userEnteredFormat': {'textFormat': {'bold': True}}}
+        fields = 'userEnteredFormat.textFormat.bold'
+        self.apply_formatting_to_cell(cell_range, cell, fields)
+
